82aad8b37a17fd40d56e3b5b4524f66c6cb78878
Claudio Cristian Lopez Linco <lopezlin@kyndryl.com>
2023-12-07 10:30:05 -0300

    Servlet se conecta a la BBDD

---
+++ src/main/java/com/garage/conexion/Servlet.java
@@ -0,0 +1,49 @@
+package com.garage.conexion;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/Servlet")
+public class Servlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hola Mundo desde el Servlet";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/bbddgarageseptiembre23?useSSL=false";
+        String usuario = "root";
+        String password = "Unicamente1";
+
+        String driver = "com.mysql.jdbc.Driver";
+
+        try {
+
+            PrintWriter out=response.getWriter(); // codigo explido en video de Servlet
+
+            out.print("Nombre de la BBDD: " + jdbcUrl);
+
+            Class.forName(driver);	// cargamos driver jdbc
+
+            Connection myConnection = (Connection) DriverManager.getConnection(jdbcUrl, usuario, password);
+
+            out.print("\n\n\nConectado al Garage 7/10/23!!!");
+
+            myConnection.close();
+
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+
+            <title>     Index JSP - Hola mundo      </title>
+
+</head>
+<body>
+
+            <h1><%= "H1 desde el index JSP" %></h1>
+            <br/>
+            <a href="Servlet">Hola desde el Index</a>
+</body>
+</html>
